4d2c1258c7b4777e0a181b2aafeaeb5e3ad044f8
chiefsmurph <chiefsmurph@gmail.com>
2019-10-27 17:38:45 -0500

    path

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const http = require('http').Server(app)
 const io = require('socket.io');
+const path = require('path');
 
 const getRecentUploads = require('./get-recent-uploads');
 
