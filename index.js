0978ed574b980f004f749f97af715aa36bc56852
chiefsmurph <chiefsmurph@gmail.com>
2019-10-27 16:12:05 -0500

    basic file upload

--- index.js
+++ index.js
@@ -13,6 +13,25 @@ socket.on('connection', socket => {
   });
 });
 
+const fileUpload = require('express-fileupload');
+app.use(fileUpload());
+app.post('/upload', function(req, res) {
+  if (!req.files || Object.keys(req.files).length === 0) {
+    return res.status(400).send('No files were uploaded.');
+  }
+
+  // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
+  let audioFile = req.files.audioFile;
+
+  // Use the mv() method to place the file somewhere on your server
+  audioFile.mv('./uploads', err => {
+    if (err)
+      return res.status(500).send(err);
+
+    res.send('File uploaded!');
+  });
+});
+
 http.listen(port, () => {
   console.log('connected to port: '+ port)
 });
