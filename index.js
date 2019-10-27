a01c5003fd70644f288823f1fa7dab3e1cc680d9
chiefsmurph <chiefsmurph@gmail.com>
2019-10-27 16:49:02 -0500

    log

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.post('/upload', function(req, res) {
 
   // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
   let audioFile = req.files.audioFile;
-
+  console.log('got', audioFile)
   // Use the mv() method to place the file somewhere on your server
   audioFile.mv('./uploads', err => {
     if (err)
