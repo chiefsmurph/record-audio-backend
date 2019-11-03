07a95a063dbef6d903c285c73d77184f2f6575ff
chiefsmurph <chiefsmurph@gmail.com>
2019-11-03 09:59:38 -0600

    try

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ socket.on('connection', async socket => {
   socket.on('client:login', async (data, cb) => {
     console.log({ data }, 'login')
     cb(
-      await User.login(data)
+      await User.call(User, data)
     )
   });
 
