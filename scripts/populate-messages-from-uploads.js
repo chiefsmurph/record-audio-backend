8714b56e94f4a1227f15a9ada7c61cdd6db18901
chiefsmurph <chiefsmurph@gmail.com>
2019-11-07 17:23:59 -0600

    what

--- scripts/populate-messages-from-uploads.js
+++ scripts/populate-messages-from-uploads.js
@@ -10,11 +10,13 @@ const getRecentUploads = require('../actions/get-recent-uploads');
     console.log({ upload })
     const [fileName, fileType] = upload.split('.');
     const [username, name] = fileName.split(' - ');
+
+    console.log({ username, name, fileName, fileType})
     if (!username || !name) {
       console.log('unable to handle', upload);
       continue;
     }
-    
+    console.log({ username, name, fileName, fileType})
     await Message.create({
       user: await User.findOne({ username }),
       name,
